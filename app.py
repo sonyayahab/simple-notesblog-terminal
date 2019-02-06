47d42545380dd44ebbd85f689db525f8a910d3a2
Sony Wicaksono <sonyayahab@gmail.com>
2019-02-06 23:31:48 +0700

    adding ex method implementation

--- app.py
+++ app.py
@@ -8,8 +8,13 @@ post = Post(1,
             "This is the second content",
             "Sony Wicaksono")
 
+# It will save the post into the mongo database
 post.save_to_mongo()
-posts = post.from_blog(1)
 
+# It will print out into the terminal about the post that have blog_id = 1
+posts = post.from_blog(1)
 for post in posts:
     print (post)
+
+# It will print out into terminal which post that have id
+postid = post.from_mongo("5c5af7ee2f5ac2381a1bea53")
